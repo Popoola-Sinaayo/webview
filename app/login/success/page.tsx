d78f57829fd2cf5cbf5ab37635f1cdca69018063
Popoola_Sinaayo <olusegunpopoola4real@gmail.com>
2025-04-08 14:12:26 +0100

    update on mobile app

---
+++ app/login/success/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useEffect } from "react";
+import styles from "./page.module.css";
+import LoadingIcons from "react-loading-icons";
+import { useSearchParams } from "next/navigation";
+
+declare global {
+  interface Window {
+    ReactNativeWebView: {
+      postMessage: (message: string) => void;
+    };
+  }
+}
+function SuccessPage() {
+  const params = useSearchParams()
+  useEffect(() => {
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ status: "success", message: "Login Successful", token: params.get("token") })
+      );
+    }
+    const token = params.get("token")
+    console.log(token)
+    
+    
+    window.close(); // Closes the WebView
+  }, []);
+
+  return (
+    <div className={styles.main}>
+      <LoadingIcons.TailSpin stroke="#7216F3" />
+    </div>
+  );
+}
+
+const PageWithSuspense = () => {
+  return (
+    <React.Suspense fallback={<LoadingIcons.TailSpin stroke="#7216F3" />}>
+      <SuccessPage />
+    </React.Suspense>
+  )
+}
+
+export default PageWithSuspense;
